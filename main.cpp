61856377612c1e120d4ee3ee9c9f6f60d101100d
Kenny Luu <kluu006@ucr.edu>
2016-10-23 01:47:27 -0700

    a* misplaced tiles implemented

--- main.cpp
+++ main.cpp
@@ -81,6 +81,27 @@ int heur(vector<vector<int> > puzzle, map<int, pair <int,int> > puzzle_map){
 	return value;
 }
 
+int misplaced_tiles(vector<vector<int> > puzzle){
+	int value = 0;
+	for(int i = 0; i < 3; i++){
+		for(int j = 0; j < 3; j++){
+			int current = (i * 3) + j + 1;
+			bool not_tile = false;
+			if(puzzle.at(i).at(j) == 0)
+				not_tile = true;
+			int actual = puzzle.at(i).at(j);
+			//cout << spot.first << "," << spot.second << endl;
+			//cout << actual.first << "," << actual.second << endl;
+			if(actual != current){
+				if(!not_tile)
+					value++;
+			}
+				
+		}
+	}
+	return value;
+}
+
 // Swaps the sections
 // Is preformed when going down a node
 
@@ -308,6 +329,106 @@ void BFS(vector<vector<int> > puzzle, map<int, pair<int,int> > puzzle_map, map<i
 
 }
 
+vector<vector<int> > a_star_misplaced(vector<vector<int> > puzzle, map<int, pair<int,int> > puzzle_map, map<int, pair<int,int> > directions, int& b_spot, int level){
+	pair<int,int> curr_directions = directions[b_spot];
+	int up=0, down=0, right=0, left=0;
+	int b_spot1 = b_spot;
+	int b_spot2 = b_spot;
+	int b_spot3 = b_spot;
+	int b_spot4 = b_spot;
+	Node node_right; 
+	Node node_left;
+	Node node_up;
+	Node node_down;
+	node_right.index = level;
+	node_left.index = level;
+	node_up.index = level;
+	node_down.index = level;
+	vector<vector<int> > ultimate_puzzle;
+	priority_queue<Node, vector<Node>, least_distance> tree_copy;
+	switch(curr_directions.first){
+		case 1 : // right
+			node_right.direction = "R";
+			node_right.distance = misplaced_tiles(swap_places(puzzle, puzzle_map, b_spot1, b_spot1 + 1));
+			break;
+		case 2 : // left
+			node_left.direction = "L";
+			node_left.distance = misplaced_tiles(swap_places(puzzle, puzzle_map, b_spot2, b_spot2 - 1));
+			break;
+		case 3 : // both
+			node_right.direction = "R";
+			node_right.distance = misplaced_tiles(swap_places(puzzle, puzzle_map, b_spot1, b_spot1 + 1));
+			
+			node_left.direction = "L";
+			node_left.distance = misplaced_tiles(swap_places(puzzle, puzzle_map, b_spot2, b_spot2 - 1));
+			break;
+		default:
+			break;
+	}
+	switch(curr_directions.second){
+		case 1 : // up
+			node_up.direction = "U";
+			node_up.distance = misplaced_tiles(swap_places(puzzle, puzzle_map, b_spot3, b_spot3 - 3));
+			break;
+		case 2 : // down
+			node_down.direction = "D";
+			node_down.distance = misplaced_tiles(swap_places(puzzle, puzzle_map, b_spot4, b_spot4 + 3));
+			break;
+		case 3 : // both
+			node_up.direction = "U";
+			node_up.distance = misplaced_tiles(swap_places(puzzle, puzzle_map, b_spot3, b_spot3 - 3));
+			node_down.direction = "D";
+			node_down.distance = misplaced_tiles(swap_places(puzzle, puzzle_map, b_spot4, b_spot4 + 3));
+			break;
+		default:
+			break;
+
+	}
+	if(node_right.distance != -1)
+		tree.push(node_right);
+	if(node_left.distance != -1)
+		tree.push(node_left);
+	if(node_up.distance != -1)
+		tree.push(node_up);
+	if(node_down.distance != -1)
+		tree.push(node_down);
+	//cout << "-----------------\n" << node_right.direction << endl << node_right.distance << endl;
+	//cout << "-----------------\n" << node_left.direction << endl << node_left.distance << endl;
+	//cout << "-----------------\n" << node_up.direction << endl << node_up.distance << endl;
+	//cout << tree.top().direction << endl << tree.top().distance << endl;
+	if(tree.top().direction == "R")
+		ultimate_puzzle = swap_places(puzzle, puzzle_map, b_spot, b_spot + 1);
+	else if(tree.top().direction == "L")
+		ultimate_puzzle = swap_places(puzzle, puzzle_map, b_spot, b_spot - 1);
+	else if(tree.top().direction == "U")
+		ultimate_puzzle = swap_places(puzzle, puzzle_map, b_spot, b_spot - 3);
+	else if(tree.top().direction == "D")
+		ultimate_puzzle = swap_places(puzzle, puzzle_map, b_spot, b_spot + 3);
+		
+	//cout << tree_level.top().direction << endl << tree_level.top().distance << endl;
+	tree_copy = tree;
+	
+	bool one = true;
+	int k = 0;
+	while(!tree.empty()){
+		string dir = tree.top().direction;
+		printf("(Direction:%s, Distance: %d, Depth: %d)\n", dir.c_str(), tree.top().distance, tree.top().index);
+		if(k)
+			real_tree.push_back(tree.top());
+		tree.pop();
+		k++;
+	}
+	cout << "POPPED\n" << endl;
+	reverse(real_tree.begin(), real_tree.end());
+	for(int i = 0; i < real_tree.size(); i++){
+		string dir = real_tree.at(i).direction;
+		printf("(Direction:%s, Distance: %d, Depth: %d)\n", dir.c_str(), real_tree.at(i).distance, real_tree.at(i).index);
+	}
+	
+	return ultimate_puzzle;
+}
+
+
 int main(){
 	vector<vector<int> > puzzle;
 	puzzle.resize(3, vector<int>(0,0));
@@ -315,11 +436,11 @@ int main(){
 	puzzle.at(0).push_back(2);
 	puzzle.at(0).push_back(3);
 	puzzle.at(1).push_back(4);
-	puzzle.at(1).push_back(5);
 	puzzle.at(1).push_back(0);
+	puzzle.at(1).push_back(6);
 	puzzle.at(2).push_back(7);
+	puzzle.at(2).push_back(5);
 	puzzle.at(2).push_back(8);
-	puzzle.at(2).push_back(6);
 
 	int level = 1;
 
@@ -359,6 +480,8 @@ int main(){
 		level += 1;
 		cout << "FINAL: " << value << endl;
 	}*/
+
+	/*
 	Node root;
 	root.puzzle = puzzle;
 	root.b_spot = b_spot;
@@ -383,6 +506,18 @@ int main(){
 		cout << "LEVEL: " << level << endl;
 		cout << endl;
 
+	}*/
+	cout << misplaced_tiles(puzzle) << endl;
+	while(value != 0){
+	//for(int k = 0; k < 5; k++){
+		puzzle = a_star_misplaced(puzzle, puzzle_map, directions, b_spot, level);
+		value = misplaced_tiles(puzzle);
+		for(int i = 0; i < 3; i++){
+			for(int j = 0; j < 3; j++)
+				cout << puzzle.at(i).at(j) << endl;
+		}
+		level += 1;
+		cout << "FINAL: " << value << endl;
 	}
 	return 0;
 }
