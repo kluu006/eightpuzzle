c37d2589f74018608390f84cd2fc571cd5f12959
Kenny Luu <kluu006@ucr.edu>
2016-10-24 03:05:42 -0700

    sort of fixed looping for A* manhattan

--- main.cpp
+++ main.cpp
@@ -135,6 +135,8 @@ struct least_distance{
 
 priority_queue<Node, vector<Node>, least_distance> tree;
 vector<Node> real_tree;
+vector<Node> popped;
+int go_back = 0;
 
 vector<vector<int> > next_node(vector<vector<int> > puzzle, map<int, pair<int,int> > puzzle_map, map<int, pair<int,int> > directions, int& b_spot, int level){
 	pair<int,int> curr_directions = directions[b_spot];
@@ -158,15 +160,15 @@ vector<vector<int> > next_node(vector<vector<int> > puzzle, map<int, pair<int,in
 	switch(curr_directions.first){
 		case 1 : // right
 			node_right.direction = "R";
-			node_right.distance = heur(swap_places(puzzle, puzzle_map, b_spot1, b_spot1 + 1), puzzle_map);
+			node_right.distance = heur(node_right.puzzle=swap_places(puzzle, puzzle_map, b_spot1, b_spot1 + 1), puzzle_map);
 			break;
 		case 2 : // left
 			node_left.direction = "L";
-			node_left.distance = heur(swap_places(puzzle, puzzle_map, b_spot2, b_spot2 - 1), puzzle_map);
+			node_left.distance = heur(node_left.puzzle=swap_places(puzzle, puzzle_map, b_spot2, b_spot2 - 1), puzzle_map);
 			break;
 		case 3 : // both
 			node_right.direction = "R";
-			node_right.distance = heur(new_puzzle=swap_places(puzzle, puzzle_map, b_spot1, b_spot1 + 1), puzzle_map);
+			node_right.distance = heur(node_right.puzzle=swap_places(puzzle, puzzle_map, b_spot1, b_spot1 + 1), puzzle_map);
 			
 			/*
 			for(int i = 0; i < 3; i++){
@@ -174,7 +176,7 @@ vector<vector<int> > next_node(vector<vector<int> > puzzle, map<int, pair<int,in
 					cout << new_puzzle.at(i).at(j) << endl;
 			}*/
 			node_left.direction = "L";
-			node_left.distance = heur(new_pizzle=swap_places(puzzle, puzzle_map, b_spot2, b_spot2 - 1), puzzle_map);
+			node_left.distance = heur(node_left.puzzle=swap_places(puzzle, puzzle_map, b_spot2, b_spot2 - 1), puzzle_map);
 			/*
 			for(int i = 0; i < 3; i++){
 				for(int j = 0; j < 3; j++)
@@ -187,38 +189,66 @@ vector<vector<int> > next_node(vector<vector<int> > puzzle, map<int, pair<int,in
 	switch(curr_directions.second){
 		case 1 : // up
 			node_up.direction = "U";
-			node_up.distance = heur(swap_places(puzzle, puzzle_map, b_spot3, b_spot3 - 3), puzzle_map);
+			node_up.distance = heur(node_up.puzzle=swap_places(puzzle, puzzle_map, b_spot3, b_spot3 - 3), puzzle_map);
 			break;
 		case 2 : // down
 			node_down.direction = "D";
-			node_down.distance = heur(swap_places(puzzle, puzzle_map, b_spot4, b_spot4 + 3), puzzle_map);
+			node_down.distance = heur(node_down.puzzle=swap_places(puzzle, puzzle_map, b_spot4, b_spot4 + 3), puzzle_map);
 			break;
 		case 3 : // both
 			node_up.direction = "U";
-			node_up.distance = heur(new_puzzle=swap_places(puzzle, puzzle_map, b_spot3, b_spot3 - 3), puzzle_map);
-			for(int i = 0; i < 3; i++){
-				for(int j = 0; j < 3; j++)
-					cout << new_puzzle.at(i).at(j) << endl;
-			}
+			node_up.distance = heur(node_up.puzzle=swap_places(puzzle, puzzle_map, b_spot3, b_spot3 - 3), puzzle_map);
 			node_down.direction = "D";
-			node_down.distance = heur(new_pizzle=swap_places(puzzle, puzzle_map, b_spot4, b_spot4 + 3), puzzle_map);
-			for(int i = 0; i < 3; i++){
-				for(int j = 0; j < 3; j++)
-					cout << new_pizzle.at(i).at(j) << endl;
-			}
+			node_down.distance = heur(node_down.puzzle=swap_places(puzzle, puzzle_map, b_spot4, b_spot4 + 3), puzzle_map);
 			break;
 		default:
 			break;
 
 	}
-	if(node_right.distance != -1)
-		tree.push(node_right);
+	bool went_right = false;
+	bool went_left = false;
+	bool went_up = false;
+	bool went_down = false;
+	for(int i = 0; i < popped.size(); i++){
+		/*
+		cout << "--------------" << endl;
+		for(int k = 0; k < 3; k++){
+			for(int j = 0; j < 3; j++)
+				cout << popped.at(i).puzzle.at(k).at(j) << endl;
+		}*/
+		/*
+		cout << "=============" << endl;
+		for(int k = 0; k < 3; k++){
+			for(int j = 0; j < 3; j++)
+				cout << node_down.puzzle.at(k).at(j) << endl;
+		}
+		cout << "--------------" << endl;
+		*/
+		if(popped.at(i).puzzle == node_right.puzzle && popped.at(i).direction == node_right.direction)
+			went_right = true;
+		else if(node_right.distance != -1 && i == popped.size()-1 && !went_right)
+			tree.push(node_right);
+		if(popped.at(i).puzzle == node_left.puzzle && popped.at(i).direction == node_left.direction)
+			went_left = true;
+		else if(node_left.distance != -1 && i == popped.size()-1 && !went_left)
+			tree.push(node_left);
+		if(popped.at(i).puzzle == node_up.puzzle && popped.at(i).direction == node_up.direction)
+			went_up = true;
+		else if(node_up.distance != -1 && i == popped.size()-1 && !went_up)
+			tree.push(node_up);
+		if(popped.at(i).puzzle == node_down.puzzle && popped.at(i).direction == node_down.direction)
+			went_down = true;
+		else if(node_down.distance != -1 && i == popped.size()-1 && !went_down)
+			tree.push(node_down);
+	}
+	/*
 	if(node_left.distance != -1)
 		tree.push(node_left);
 	if(node_up.distance != -1)
 		tree.push(node_up);
 	if(node_down.distance != -1)
 		tree.push(node_down);
+	*/
 	//cout << "-----------------\n" << node_right.direction << endl << node_right.distance << endl;
 	//cout << "-----------------\n" << node_left.direction << endl << node_left.distance << endl;
 	//cout << "-----------------\n" << node_up.direction << endl << node_up.distance << endl;
@@ -237,20 +267,42 @@ vector<vector<int> > next_node(vector<vector<int> > puzzle, map<int, pair<int,in
 	
 	bool one = true;
 	int k = 0;
+	if(tree.empty()){
+		cout << "back" << endl;
+		if(go_back == 0){
+			ultimate_puzzle = popped.at(0).puzzle;
+			b_spot = popped.at(0).b_spot;
+			go_back = popped.size();
+		}
+		else if(level == popped.size()){
+			ultimate_puzzle = popped.at(0).puzzle;
+			b_spot = popped.at(0).b_spot;
+			go_back = popped.size();
+		}
+		else if(go_back > 0){
+			ultimate_puzzle = popped.at(level).puzzle;
+			b_spot = popped.at(level).b_spot;
+			go_back = level;
+		}
+	}
+	
 	while(!tree.empty()){
 		string dir = tree.top().direction;
 		printf("(Direction:%s, Distance: %d, Depth: %d)\n", dir.c_str(), tree.top().distance, tree.top().index);
 		if(k)
 			real_tree.push_back(tree.top());
+		else
+			popped.push_back(tree.top());
 		tree.pop();
 		k++;
 	}
+	/*
 	reverse(real_tree.begin(), real_tree.end());
 	for(int i = 0; i < real_tree.size(); i++){
 		string dir = real_tree.at(i).direction;
 		cout << "wowowo" << endl;
 		printf("(Direction:%s, Distance: %d, Depth: %d)\n", dir.c_str(), real_tree.at(i).distance, real_tree.at(i).index);
-	}
+	}*/
 	
 	return ultimate_puzzle;
 }
@@ -433,16 +485,16 @@ int main(){
 	vector<vector<int> > puzzle;
 	puzzle.resize(3, vector<int>(0,0));
 	puzzle.at(0).push_back(1);
-	puzzle.at(0).push_back(2);
+	puzzle.at(0).push_back(5);
 	puzzle.at(0).push_back(3);
 	puzzle.at(1).push_back(4);
 	puzzle.at(1).push_back(0);
 	puzzle.at(1).push_back(6);
 	puzzle.at(2).push_back(7);
-	puzzle.at(2).push_back(5);
+	puzzle.at(2).push_back(2);
 	puzzle.at(2).push_back(8);
 
-	int level = 1;
+	int level = 0;
 
 	int b_spot = initial_b_spot(puzzle);
 	map<int, pair<int,int> >puzzle_map = end_puzzle();
@@ -468,18 +520,28 @@ int main(){
 	//cout << "-------------------" << endl;
 	//cout << value << endl;
 	
-	/*
+	Node root;
+	root.puzzle = puzzle;
+	root.b_spot = b_spot;
+	root.index = 0;
+	popped.push_back(root);
+
 	while(value != 0){
-	//for(int k = 0; k < 5; k++){
+	//for(int k = 0; k < 10000; k++){
 		puzzle = next_node(puzzle, puzzle_map, directions, b_spot, level);
+		if(puzzle == root.puzzle)
+			level = 0;
 		value = heur(puzzle, puzzle_map);
+		
 		for(int i = 0; i < 3; i++){
 			for(int j = 0; j < 3; j++)
 				cout << puzzle.at(i).at(j) << endl;
 		}
 		level += 1;
 		cout << "FINAL: " << value << endl;
-	}*/
+		cout << level << endl;
+		cout << "POPPED_SZ: " << popped.size() << endl;
+	}
 
 	/*
 	Node root;
@@ -507,6 +569,7 @@ int main(){
 		cout << endl;
 
 	}*/
+	/*
 	cout << misplaced_tiles(puzzle) << endl;
 	while(value != 0){
 	//for(int k = 0; k < 5; k++){
@@ -518,7 +581,7 @@ int main(){
 		}
 		level += 1;
 		cout << "FINAL: " << value << endl;
-	}
+	}*/
 	return 0;
 }
 
